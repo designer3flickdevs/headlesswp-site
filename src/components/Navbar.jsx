bd7a6a6fcb2effc42604f1f51a69c0016cac1d33
designer3flickdevs <designer3flickdevs@gmail.com>
2025-10-16 13:33:10 +0530

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,30 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 
 const Navbar = () => {
+const [menuItems, setMenuItems] = useState([]);
+
+  useEffect(() => {
+    // Fetch menu from WordPress API
+    fetch("http://localhost/headless_wordpress/server/wp-json/wp-api-menus/v2/menus/17")
+      .then((response) => response.json())
+      .then((data) => {
+        // WordPress menus usually store items in `items` array
+        if (data.items) {
+          setMenuItems(data.items);
+        }
+      })
+      .catch((error) => console.error("Error fetching menu:", error));
+  }, []);
+
   return (
     <div className="flex justify-between items-center h-24 max-w-[1240] max-auto px-5 text-white">
         <h1 className="w-full text-3xl font-bold text-[#00df9a]">REACT</h1>
         <ul className="flex">
-            <li className="p-4">Home</li>
-            <li className="p-4">Company</li>
-            <li className="p-4">Resources</li>
-            <li className="p-4">About</li>
-            <li className="p-4">Contact</li>
+            {menuItems.map((item) => (
+                <li key={item.ID} className="p-4">  
+                    <a href={item.url}>{item.title}</a>
+                </li>
+            ))}
         </ul>
     </div>
   )
