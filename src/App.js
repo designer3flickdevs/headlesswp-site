7e664d2f12e18354f10aebd19eb0bf7e8b1899d6
designer3flickdevs <designer3flickdevs@gmail.com>
2025-10-16 11:56:23 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-red-500 p-5">
+      Tailwind CSS is working!
+    </div>
+  );
+}
+
+export default App;
+
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Scan all React source files
+  ],
+  theme: {
+    extend: {}, // Custom colors, fonts, spacing etc. later add કરો
+  },
+  plugins: [],
+}
