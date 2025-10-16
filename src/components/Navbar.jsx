eac144fcbedf93bc6ee6468a5953a50c367a9d22
designer3flickdevs <designer3flickdevs@gmail.com>
2025-10-16 15:54:47 +0530

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,7 +36,7 @@ const handleNav = () => {
             {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
         </div>
         <ul className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
-            <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>REACT.</h1>
+            <h1 className='text-3xl font-bold text-[#00df9a] m-4'>REACT.</h1>
             {menuItems.map((item) => (
                 <li key={item.ID} className='p-4 border-b border-gray-600'>  
                     <a href={item.url}>{item.title}</a>
