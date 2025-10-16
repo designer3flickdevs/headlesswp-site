9958aaa17c37db5232f67e6b4dcc8dc33d16d653
designer3flickdevs <designer3flickdevs@gmail.com>
2025-10-16 12:13:08 +0530

    update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="text-3xl font-bold text-red-500 p-5">
-      Tailwind CSS is working!
+      Tailwind CSS is working122
     </div>
   );
 }
