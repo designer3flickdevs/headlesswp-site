6a1a36b483ce97258b4968c62122de0bc6ab314c
designer3flickdevs <designer3flickdevs@gmail.com>
2025-10-23 10:21:53 +0530

    footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Footer from './components/Footer';
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Footer />
     </div>
   );
 }
