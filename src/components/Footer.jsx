a8f5f4261e29580fd72b7b1b7cc7a8f694b874ce
designer3flickdevs <designer3flickdevs@gmail.com>
2025-10-23 16:21:10 +0530

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -42,12 +42,13 @@ const Footer = () => {
       <div className='lg:col-span-2 flex justify-between mt-6'>
     <div>
         <h6 className='font-medium text-gray-400'>Solutions</h6>
-        <ul>
-            <li className='py-2 text-sm'>Analytics</li>
-            <li className='py-2 text-sm'>Marketing</li>
-            <li className='py-2 text-sm'>Commerce</li>
-            <li className='py-2 text-sm'>Insights</li>
-        </ul>
+        <ul className="hidden md:flex">
+        {footerMenu.map((item) => (
+          <li key={item.ID} className="p-4">
+            <a href={item.url}>{item.title}</a>
+          </li>
+        ))}
+      </ul>
     </div>
     <div>
         <h6 className='font-medium text-gray-400'>Support</h6>
