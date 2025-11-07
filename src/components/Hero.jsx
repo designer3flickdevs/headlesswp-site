19d86cdf48d67a5c52dee4a7a87f48e2809fa3c6
designer3flickdevs <designer3flickdevs@gmail.com>
2025-11-07 10:59:51 +0530

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
 
-const Hero = () => {
+const Hero = ({props}) => {
   return (
     <div className='text-white'>
       <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
         <p className='text-[#00df9a] font-bold p-2'>
-          GROWING WITH DATA ANALYTICS
+          GROWING WITH DATA ANALYTICS  {props.data}
         </p>
         <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>
           Grow with data.
